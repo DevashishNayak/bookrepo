7aa850bde0d8238d355c221e13cb3fd3bae3d9f6
Devashish Nayak <devashishnayakrps@gmail.com>
2021-06-12 13:34:02 +0530

    bookrepo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import axios from "axios";
+import { Container, AppBar, Toolbar, Button } from "@material-ui/core/";
+import { DataGrid } from "@material-ui/data-grid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [books, setBooks] = useState([]);
+
+  const columns = [
+    { field: "id", headerName: "Book ID", type: "number", width: 110 },
+    { field: "title", headerName: "Title", width: 410 },
+    { field: "authors", headerName: "Author", width: 210 },
+    {
+      field: "average_rating",
+      headerName: "Average Rating",
+      type: "number",
+      width: 150,
+    },
+    { field: "isbn", headerName: "ISBN", width: 120 },
+    { field: "language_code", headerName: "Language", width: 120 },
+    {
+      field: "ratings_count",
+      headerName: "Rating Count",
+      type: "number",
+      width: 140,
+    },
+    { field: "price", headerName: "Price", type: "number", width: 90 },
+  ];
+
+  const rows = books.map(
+    ({
+      bookID: id,
+      title,
+      authors,
+      average_rating,
+      isbn,
+      language_code,
+      ratings_count,
+      price,
+    }) => ({
+      id,
+      title,
+      authors,
+      average_rating,
+      isbn,
+      language_code,
+      ratings_count,
+      price,
+    })
   );
-}
+
+  const hook = () => {
+    axios
+      .get(
+        "https://s3-ap-southeast-1.amazonaws.com/he-public-data/books8f8fe52.json"
+      )
+      .then((response) => {
+        setBooks(response.data);
+      });
+  };
+
+  useEffect(hook, []);
+
+  return (
+    <Container>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit" component={Link} to="/">
+            home
+          </Button>
+          <Button color="inherit" component={Link} to="/checkout">
+            cart
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Switch>
+        <Route path="/checkout">
+          <div></div>
+        </Route>
+        <Route path="/">
+          <div style={{ height: 670, width: "100%" }}>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              pageSize={10}
+              checkboxSelection
+              showToolbar
+            />
+          </div>
+        </Route>
+      </Switch>
+    </Container>
+  );
+};
 
 export default App;
